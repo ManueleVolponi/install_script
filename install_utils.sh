cc08bb8dd4134c42ace019973f112ee34f6dd033
ManueleV <75172914+ManueleVolponi@users.noreply.github.com>
2024-09-22 12:46:26 +0200

    add: composer installation, fixes on choices

--- install_utils.sh
+++ install_utils.sh
@@ -4,11 +4,11 @@
 # it will check if the package is already installed or not before installing it.
 
 packages=(
-    "mysql, mysql -V, sudo apt update && sudo apt install -y mysql-server"
-    "curl, curl --version, sudo apt update && sudo apt install -y curl"
-    "nvm, nvm --version, curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash && nvm --version && nvm install --lts"
-    "apache2, apache2 -v, sudo apt update && sudo apt install -y apache2"
-    "git, git --version, sudo apt update && sudo apt install -y git"
+    "mysql, mysql, sudo apt update && sudo apt install -y mysql-server"
+    "curl, curl, sudo apt update && sudo apt install -y curl"
+    "nvm, nvm, curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash && nvm --version && nvm install --lts"
+    "apache2, apache2, sudo apt update && sudo apt install -y apache2"
+    "git, git, sudo apt update && sudo apt install -y git"
 )
 
 install_package() {
@@ -22,17 +22,15 @@ install_package() {
         return 1
     fi
 
-    if [ "$PACKAGE_NAME" == "nvm" ]
+    if [[ "$PACKAGE_NAME" == "nvm" ]]
     then
-        if [ -f "$HOME/.nvm/nvm.sh" ]
-        then
+        if [ -f "$HOME/.nvm/nvm.sh" ]; then
             echo "$PACKAGE_NAME is already installed."
             return 0
         fi
     fi
 
-    if $COMMAND_CHECK &> /dev/null
-    then
+    if command -v $COMMAND_CHECK &> /dev/null; then
         echo "$PACKAGE_NAME is already installed."
     else
         echo "Installing $PACKAGE_NAME..."
@@ -52,57 +50,63 @@ done
 echo "Do you want to install Python, PHP, or both? (Enter: python/php/both)"
 read -r INSTALL_CHOICE
 
+# if user enter something other than python, php, or both
+if [[ "$INSTALL_CHOICE" != "python" && "$INSTALL_CHOICE" != "php" && "$INSTALL_CHOICE" != "both" && -n "$INSTALL_CHOICE" ]]; then
+    echo "Invalid choice. Please enter python, php, or both."
+    exit 1
+fi
+
 # install Both Python and PHP
- if [[ -z "$INSTALL_CHOICE" ]] || ["$INSTALL_CHOICE" == "both" ]; then
+if [[ -z "$INSTALL_CHOICE"  || "$INSTALL_CHOICE" == "both" ]]; then
     PY_INSTALLED=0
     PHP_INSTALLED=0
 
-    if python3 --version &> /dev/null
-    then
+    if python3 --version &> /dev/null; then
         echo "Python is already installed."
         PY_INSTALLED=1
     fi
 
-    if php -v &> /dev/null
-    then
+    if php -v &> /dev/null; then
         echo "PHP is already installed."
         PHP_INSTALLED=1
     fi
 
-    if [ "$PY_INSTALLED" == 0 ]; then
+    if [[ "$PY_INSTALLED" == 0 ]]; then
         sudo apt update && sudo apt install -y python3 python3-pip
         echo "Python has been installed."
     fi
 
-    if [ "$PHP_INSTALLED" == 0 ]; then
+    if [[ "$PHP_INSTALLED" == 0 ]]; then
         sudo apt update && sudo apt install -y php
         echo "PHP has been installed."
     fi
 
-    echo "Python and PHP are installed."
     python3 --version && php -v
- fi
+fi
 
 #install Python
 if [ "$INSTALL_CHOICE" == "python" ]; then
-    if python3 --version &> /dev/null
-    then
-        echo "Python is already installed."
-    else
-        sudo apt update && sudo apt install -y python3 python3-pip
-        echo "Python has been installed."
-    fi
+    install_package "python3" "python3" "sudo apt update && sudo apt install -y python3 python3-pip"
     python3 --version
 fi
 
 #install PHP
 if [ "$INSTALL_CHOICE" == "php" ]; then
-    if php -v &> /dev/null
-    then
-        echo "PHP is already installed."
-    else
-        sudo apt update && sudo apt install -y php
-        echo "PHP has been installed."
-    fi
+    install_package "php" "php" "sudo apt update && sudo apt install -y php"
     php -v
+fi
+
+# Ask if user wants to install Composer
+echo "Do you want to install Composer? (Enter: yes/no)"
+read -r INSTALL_COMPOSER
+
+if [ "$INSTALL_COMPOSER" == "yes" ]; then
+    if [ -f "/usr/local/bin/composer" ]; then
+        echo "Composer is already installed."
+    else
+        echo "Installing Composer..."
+        curl -sS https://getcomposer.org/installer | php
+        sudo mv composer.phar /usr/local/bin/composer
+        echo "Composer has been installed."
+    fi
 fi
